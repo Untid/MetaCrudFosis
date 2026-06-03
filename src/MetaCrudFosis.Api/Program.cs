0449278862a2d069a4305544aebe123f1fc581db
unknown <javier_otero98@hotmail.com>
2026-06-03 13:17:39 +0200

    Fase 1: configuracion CORS para la Web MVC

--- src/MetaCrudFosis.Api/Program.cs
+++ src/MetaCrudFosis.Api/Program.cs
@@ -14,6 +14,12 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddSingleton<ILogRepository, LogRepository>();
 
+builder.Services.AddCors(options =>
+    options.AddPolicy("PermitirWeb", policy =>
+        policy.WithOrigins("http://localhost:5002")  // origen de la Web MVC. Ajustable.
+              .AllowAnyHeader()
+              .AllowAnyMethod()));
+
 builder.Services
     .AddControllers(options =>
         options.Conventions.Add(new GenericControllerRouteConvention()))
@@ -35,7 +41,8 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
     app.MapScalarApiReference();
 }
-    
+
+app.UseCors("PermitirWeb");
 
 app.MapControllers();
 
